3b4cc66dc443f7433745f3c48225309ddca8fa48
garizola <99741841+garizola@users.noreply.github.com>
2024-10-23 00:49:41 -0500

    adding hrefs

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -10,25 +10,36 @@ const Footer = () => {
         {/* <p className="font-bold">2024</p> */}
         <p>Giovanni Arizola</p>
       </div>
-      <a href="#" className=" text-green-500 hover:text-green-600 hover:italic">
+      <a
+        href="https://give-me-feedback.netlify.app/"
+        className=" text-green-500 hover:text-green-600 hover:italic"
+        target="_blank"
+        rel="noreferrer"
+      >
         Feedback Board
       </a>
       <div className="flex gap-3 text-lg">
         <a
           href="https://github.com/garizola"
           className="text-purple-400 hover:text-purple-700"
+          target="_blank"
+          rel="noreferrer"
         >
           <FaGithub />
         </a>
         <a
           href="https://www.linkedin.com/in/giovanniarizola/"
           className="text-blue-400 hover:text-blue-600"
+          target="_blank"
+          rel="noreferrer"
         >
           <FaLinkedin />
         </a>
         <a
           href="https://contact-giovanni.vercel.app/"
           className="text-gray-400  hover:text-gray-600"
+          target="_blank"
+          rel="noreferrer"
         >
           <IoMdMail />
         </a>
